ba1ea57624535381dc63011ab7c5686d08434770
Dmitriy Krayniy <krdima92@gmail.com>
2020-08-26 12:08:41 +0300

    merged previous changes

---
+++ destroy_mgmt.ps1
@@ -0,0 +1,20 @@
+#!/usr/bin/env pwsh
+
+param
+(
+    [Alias("c", "Path")]
+    [Parameter(Mandatory=$false, Position=0)]
+    [string] $ConfigPath
+)
+
+$ErrorActionPreference = "Stop"
+
+# Load support functions
+$rootPath = $PSScriptRoot
+if ($rootPath -eq "") { $rootPath = "." }
+. "$($rootPath)/lib/include.ps1"
+$rootPath = $PSScriptRoot
+if ($rootPath -eq "") { $rootPath = "." }
+
+# Todo: Insert here mgmt destroying...
+
